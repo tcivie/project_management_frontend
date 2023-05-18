ea60c610129e798ceab9bb08c769bfce1a815b2e
Omer <omeravi96@gmail.com>
2023-05-19 00:16:34 +0300

    Added reducers for users

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import searchReducer from './searchReducer';
+import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
     search: searchReducer,
+    user: userReducer,
     // other reducers would go here
 });
 
