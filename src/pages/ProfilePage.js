a96c4fc2944ea794715a5bd7c0bf1926479909b6
Your Name <adirmelker1@gmail.com>
2023-05-19 23:23:45 +0300

    profile page added

---
+++ src/pages/ProfilePage.js
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from 'react';
+import { Map } from 'pigeon-maps';
+import { maptiler } from 'pigeon-maps/providers';
+import { Layout, Card } from 'antd';
+import SearchBar from '../components/SearchBar';
