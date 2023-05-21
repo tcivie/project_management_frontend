77fde55b7940b84ccb7e1da76370b0fb0cf16f85
Gleb Tcivie <gleb@tcivie.com>
2023-05-21 17:33:12 +0300

    Added special drawer for user location

--- src/components/GeoLocation/MyDrawer.js
+++ src/components/GeoLocation/MyDrawer.js
@@ -1,8 +1,5 @@
 // MyDrawer.js
 
-// linter ignore max-len for all file
-/* eslint-disable max-len */
-
 import React from 'react';
 import {
   Button,
@@ -11,27 +8,19 @@ import {
   Divider,
   Descriptions,
   Statistic,
-  Skeleton, Row, Col, Badge, Avatar, Space,
+  Skeleton, Row, Col, Badge,
 } from 'antd';
 import {
-  LikeOutlined, StarOutlined, TeamOutlined, MessageOutlined,
+  TeamOutlined,
 } from '@ant-design/icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { drawerClose } from '../../redux/actions/drawerActions';
 import unicodeToEmoji from '../../utils/unicodeToEmoji';
 
-function IconText({ icon, text }) {
-  return (
-    <Space>
-      {React.createElement(icon)}
-      {text}
-    </Space>
-  );
-}
-function MyDrawer() {
+function MyDrawer(isUserLocation = false) {
   const drawerReducer = useSelector((state) => state.drawer);
   const {
-    visible, locationInfo, chatRoomInfo, stats,
+    visible, locationInfo, chatRoomInfo,
   } = drawerReducer;
   const dispatch = useDispatch();
 
@@ -46,16 +35,21 @@ function MyDrawer() {
       };
     });
   }
-
-  // const onCloseHandler = () => {
-  //   console.log('drawer closed');
-  //   dispatch(drawerClose);
-  //   console.log(visible);
-  // };
-
+  if (isUserLocation) {
+    return (
+      <Drawer
+        title="Your Location"
+        placement="right"
+        closable
+        onClose={() => dispatch(drawerClose())}
+        visible={visible}
+        size="small"
+      />
+    );
+  }
   return (
     <Drawer
-      // title={cities && cities[0] ? cities[0].name : 'no city selected'}
+            // title={cities && cities[0] ? cities[0].name : 'no city selected'}
       title={locationInfo?.location || 'no city selected'}
       placement="right"
       closable
@@ -75,19 +69,16 @@ function MyDrawer() {
           <Col span={8}>
             <Statistic
               title="Active Users"
-              // value={chatRoomInfo?.reduce((total, room) => total + room.activeUsers, 0)}
             />
           </Col>
           <Col span={8}>
             <Statistic
               title="A.Response Time"
-              // value={stats?.averageResponseTime}
             />
           </Col>
           <Col span={8}>
             <Statistic
               title="Posts Last 7 Days"
-              // value={stats?.numberOfPosts}
             />
           </Col>
         </Row>
@@ -106,7 +97,6 @@ function MyDrawer() {
             renderItem={(item) => (
               <Badge.Ribbon
                 color="volcano"
-                // text={<> <TeamOutlined /> {`${item.activeUsers} users`} </>}
                 text={<> <TeamOutlined /> ### </>}
                 status="processing"
                 style={{ right: '2px', top: '-10px' }}
@@ -133,30 +123,7 @@ function MyDrawer() {
                 padding: '16px 16px',
                 border: '1px solid #e8e8e8',
               }}
-            >
-              {/* <List */}
-              {/*  dataSource={locationInfo} */}
-              {/*  grid={{ gutter: 16, column: 1 }} */}
-              {/*  renderItem={(item) => ( */}
-              {/*    <List.Item> */}
-              {/*      <Badge.Ribbon */}
-              {/*        color="volcano" */}
-              {/*        // text={<> <TeamOutlined /> {`${item.activeUsers} users`} </>} */}
-              {/*        text={<> <TeamOutlined /> ??? users </>} */}
-              {/*        status="processing" */}
-              {/*        style={{ right: '-6px', top: '-10px' }} */}
-              {/*        hasMore={chatRoomInfo.length < 10} */}
-              {/*      > */}
-              {/*        <Button type="primary" block> */}
-              {/*          <div style={{ float: 'left' }}> */}
-              {/*            {item.name} {item.country} */}
-              {/*          </div> */}
-              {/*        </Button> */}
-              {/*      </Badge.Ribbon> */}
-              {/*    </List.Item> */}
-              {/*  )} */}
-              {/* /> */}
-            </div>
+            />
           </Col>
           <Col span={12}>
             <Divider> Trending Topics </Divider>
@@ -167,72 +134,7 @@ function MyDrawer() {
                 padding: '16px 16px',
                 border: '1px solid #e8e8e8',
               }}
-            >
-              {/* <List */}
-              {/*  dataSource={[ */}
-              {/*    { */}
-              {/*      title: 'Ant Design Title 1', */}
-              {/*      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png', */}
-              {/*      href: 'https://ant.design', */}
-              {/*      description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.', */}
-              {/*      content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.', */}
-              {/*    }, */}
-              {/*    { */}
-              {/*      title: 'Ant Design Title 2', */}
-              {/*      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png', */}
-              {/*      href: 'https://ant.design', */}
-              {/*      description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.', */}
-              {/*      content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.', */}
-              {/*    }, */}
-              {/*    { */}
-              {/*      title: 'Ant Design Title 3', */}
-              {/*      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png', */}
-              {/*      href: 'https://ant.design', */}
-              {/*      description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.', */}
-              {/*      content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.', */}
-              {/*    }, */}
-              {/*    { */}
-              {/*      title: 'Ant Design Title 4', */}
-              {/*      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png', */}
-              {/*      href: 'https://ant.design', */}
-              {/*      description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.', */}
-              {/*      content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.', */}
-              {/*    }, */}
-              {/*    { */}
-              {/*      title: 'Ant Design Title 5', */}
-              {/*      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png', */}
-              {/*      href: 'https://ant.design', */}
-              {/*      description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.', */}
-              {/*      content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.', */}
-              {/*    }, */}
-              {/*  ]} */}
-              {/*  grid={{ gutter: 16, column: 1 }} */}
-              {/*  renderItem={(item) => ( */}
-              {/*    <List.Item */}
-              {/*      key={item.title} */}
-              {/*      actions={[ */}
-              {/*        <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />, */}
-              {/*        <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />, */}
-              {/*        <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />, */}
-              {/*      ]} */}
-              {/*      extra={( */}
-              {/*        <img */}
-              {/*          width={272} */}
-              {/*          alt="logo" */}
-              {/*          src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" */}
-              {/*        /> */}
-              {/*                              )} */}
-              {/*    > */}
-              {/*      <List.Item.Meta */}
-              {/*        avatar={<Avatar src={item.avatar} />} */}
-              {/*        title={<a href={item.href}>{item.title}</a>} */}
-              {/*        description={item.description} */}
-              {/*      /> */}
-              {/*      {item.content} */}
-              {/*    </List.Item> */}
-              {/*  )} */}
-              {/* /> */}
-            </div>
+            />
           </Col>
         </Row>
       </Skeleton>
--- src/redux/actions/drawerActions.js
+++ src/redux/actions/drawerActions.js
@@ -1,6 +1,3 @@
-// eslint-disable-next-line import/prefer-default-export
-import { json } from 'react-router-dom';
-
 export default function markerClick(location, radius) {
   // eslint-disable-next-line func-names
   return async function (dispatch) {
@@ -9,7 +6,6 @@ export default function markerClick(location, radius) {
     dispatch({ type: 'DRAWER_LOCATION_INFO', payload: location });
 
     try {
-      console.log('markerClick', { point: location, radius });
       // Pull nearby cities
       const nearbyPoints = await fetch(`${process.env.REACT_APP_API_SERVER}/api/search/nearPoint`, {
         headers: {
@@ -19,14 +15,12 @@ export default function markerClick(location, radius) {
         body: JSON.stringify({ point: location, radius }),
       });
       const nearbyPointsJson = await nearbyPoints.json();
-      console.log('nearbyPoints', nearbyPointsJson);
       dispatch({ type: 'DRAWER_NEARBY_CITIES', payload: nearbyPointsJson });
       // Pull chat rooms
       const chatRooms = await fetch(`${process.env.REACT_APP_API_SERVER}/api/chat/languages`, {
         method: 'GET',
       });
       const chatRoomsJson = await chatRooms.json();
-      console.log('chatRooms', chatRoomsJson);
       dispatch({ type: 'DRAWER_CHATROOM_INFO', payload: chatRoomsJson });
       // TODO: Pull stats
     } catch (error) {
