05b47a1ad0fd3e8b5119fddf6f9db43461d462fe
itayassor236 <itayasor236@gmail.com>
2023-05-21 18:21:11 +0300

    profile page presented

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,19 +1,47 @@
 import React from 'react';
-import { Space, Button, Skeleton,Avatar } from 'antd';
-import { UserOutlined } from '@ant-design/icons';
+import { Space, Button, Skeleton, Avatar, Menu, Row, Col } from 'antd';
+import { UserOutlined, DeleteOutlined, SaveOutlined, EditOutlined } from '@ant-design/icons';
 
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+const items = [
+  getItem('Edit post', 'sub1', <EditOutlined />, [
+  ]),
+  getItem('Delete post', 'sub2', <DeleteOutlined />, [
+  ]),
+  getItem('Saved content', 'sub4', <SaveOutlined />, [
+  ]),
+];
+const onClick = (e) => {
+  console.log('click', e);
+};
 function Profile() {
   return (
     <div>
-        <Space direction="vertical" size={16}>
-    <Space wrap size={64}>
-      <Avatar size={256} icon={<UserOutlined />} />
+    <Space direction="vertical" size={16}>
+      <Space wrap size={64}>
+        <Avatar size={256} icon={<UserOutlined />} />
+      </Space>
+      <Button type="primary">Follow</Button>
+      <Button type="default">Following</Button>
+      <Button type="default">Followers</Button>
+      <Menu
+        onClick={onClick}
+        style={{
+          width: 256,
+        }}
+        mode="vertical"
+        items={items}
+      />
     </Space>
-  </Space>
-        <Button type="primary">Follow</Button>
-        <Button type="default">Following</Button>
-        <Button type="default">Followers</Button>
-    </div>
+  </div>
   );
 }
 
