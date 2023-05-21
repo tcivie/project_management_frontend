dcab80f044c94d8b6bb19779bbd1f3b119f0273b
itayassor236 <itayasor236@gmail.com>
2023-05-21 16:03:26 +0300

    profile page presented

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/proflie" element={<Profile />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,49 +1,19 @@
-import React, { useEffect, useState } from 'react';
-
-import {
-  Layout, Card,
-} from 'antd';
-import SearchBar from '../components/SearchBar';
-import MyMap from '../components/GeoLocation/MyMap';
-import Navbar from '../components/Navbar';
-
-const { Content } = Layout;
+import React from 'react';
+import { Space, Button, Skeleton,Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
 
 function Profile() {
-  const [backgroundImage, setBackgroundImage] = useState(null);
-
-  useEffect(() => {
-    fetch(
-      'https://api.unsplash.com/photos/random?client_id=5sMDfSQu2P21xA28tR-jOyjkPv9rj1l9ZniybmqaI-o&query=nature',
-    )
-      .then((response) => response.json())
-      .then((data) => setBackgroundImage(data.urls.regular))
-    // eslint-disable-next-line no-console
-      .catch((error) => console.error(error));
-  }, []);
-
   return (
-    <Layout
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
-      <Content
-        style={{
-          padding: '0 50px',
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          backdropFilter: 'blur(50%)',
-          backgroundColor: 'rgba(255, 255, 255, 0.4)',
-        }}
-      >
-        <Navbar />
-      </Content>
-    </Layout>
+    <div>
+        <Space direction="vertical" size={16}>
+    <Space wrap size={64}>
+      <Avatar size={256} icon={<UserOutlined />} />
+    </Space>
+  </Space>
+        <Button type="primary">Follow</Button>
+        <Button type="default">Following</Button>
+        <Button type="default">Followers</Button>
+    </div>
   );
 }
 
