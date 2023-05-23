2a145d552de5438845831173209928858954819c
itayassor236 <itayasor236@gmail.com>
2023-05-23 11:53:24 +0300

    profile page improvment

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,18 +1,6 @@
-import React from 'react';
-import {
-  Space,
-  Button,
-  Avatar,
-  Menu,
-} from 'antd';
-import {
-  UserOutlined,
-  DeleteOutlined,
-  SaveOutlined,
-  EditOutlined,
-  ProfileOutlined,
-  StockOutlined,
-} from '@ant-design/icons';
+import React, { useState } from 'react';
+import { Space, Button, Skeleton, Avatar, Menu, Row, Col, List } from 'antd';
+import { ProfileOutlined, DeleteOutlined, SaveOutlined, EditOutlined, StockOutlined, UserOutlined } from '@ant-design/icons';
 
 function getItem(label, key, icon, children, type) {
   return {
@@ -32,33 +20,75 @@ const items = [
   getItem('Statics', 'sub5', <StockOutlined />, []),
 ];
 
+const onClick = (e) => {
+  console.log('click', e);
+};
+
 function Profile() {
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [followersVisible, setFollowersVisible] = useState(false);
+  const [followingVisible, setFollowingVisible] = useState(false);
+
+  const handleFollowToggle = () => {
+    setIsFollowing((prevState) => !prevState);
+  };
+
+  const handleFollowersClick = () => {
+    setFollowersVisible(!followersVisible);
+    setFollowingVisible(false); // Close following list
+  };
+
+  const handleFollowingClick = () => {
+    setFollowingVisible(!followingVisible);
+    setFollowersVisible(false); // Close followers list
+  };
+
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh',
-    }}
-    >
-      <div>
-        <Space direction="vertical" size={16}>
-          <Space wrap size={64}>
+    <Row gutter={[16, 16]}>
+      <Col span={24}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Space size={128} align="center">
             <Avatar size={256} icon={<UserOutlined />} />
           </Space>
-          <Button type="primary">Follow</Button>
-          <Button type="default">Following</Button>
-          <Button type="default">Followers</Button>
-          <Menu
-            style={{
-              width: 256,
-            }}
-            mode="vertical"
-            items={items}
-          />
-        </Space>
-      </div>
-    </div>
+          <Space>
+            <Button type={isFollowing ? 'default' : 'primary'} onClick={handleFollowToggle}>
+              {isFollowing ? 'Unfollow' : 'Follow'}
+            </Button>
+            <Button type="default" onClick={handleFollowingClick}>
+              Following
+            </Button>
+            <Button type="default" onClick={handleFollowersClick}>
+              Followers
+            </Button>
+          </Space>
+        </div>
+        {followersVisible && (
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: 16 }}>
+            <List
+              header={<div>Followers</div>}
+              bordered
+              dataSource={['No followers']} // Replace with your follower data
+              renderItem={(item) => <List.Item>{item}</List.Item>}
+              style={{ width: 300 }}
+            />
+          </div>
+        )}
+        {followingVisible && (
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: 16 }}>
+            <List
+              header={<div>Following</div>}
+              bordered
+              dataSource={['No following content']} // Replace with your following data
+              renderItem={(item) => <List.Item>{item}</List.Item>}
+              style={{ width: 300 }}
+            />
+          </div>
+        )}
+      </Col>
+      <Col span={18}>
+        <Menu onClick={onClick} style={{ width: 256 }} mode="vertical" items={items} />
+      </Col>
+    </Row>
   );
 }
 
