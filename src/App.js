d3f54304cb01156a798f5dee36f1badf9e07b593
Gleb Tcivie <gleb@tcivie.com>
2023-05-31 13:14:49 +0300

    Fixed navigation bar and window sized

--- src/App.js
+++ src/App.js
@@ -8,18 +8,12 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <BrowserRouter>
-      <div style={{ height: '100vh' }}>
-        <div style={{ height: '5vh' }}>
-          <Navbar />
-        </div>
-        <div style={{ height: '95vh' }}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/chat" element={<ChatPage />} />
-          </Routes>
-        </div>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/chat" element={<ChatPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }
