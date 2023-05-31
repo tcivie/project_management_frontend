538a3eceb05ffc1929afe4c4095dd06477d1dfff
Gleb Tcivie <gleb@tcivie.com>
2023-05-31 10:55:12 +0300

    Changed the posting mechanism, added some tags functionality and rich text edit

--- src/components/Chat/Poster.js
+++ src/components/Chat/Poster.js
@@ -1,32 +1,119 @@
-import { SendOutlined } from '@ant-design/icons';
-import { Button, Form, Input } from 'antd';
+import {
+  Button, Form, Modal, Upload,
+} from 'antd';
+import {
+  SendOutlined,
+  PictureOutlined,
+  PlusCircleOutlined,
+  PlusCircleFilled,
+  PlusCircleTwoTone,
+  CloseCircleTwoTone,
+  UpCircleTwoTone, UpOutlined, CloseOutlined, UploadOutlined,
+} from '@ant-design/icons';
+import React, { useState } from 'react';
+import TextEditor from './chatComponents/TextEditor';
+import Tags from './Tags'; // Assuming TextEditor is in the same directory
 
 function Poster() {
+  const [visible, setVisible] = useState(false);
+  const [form] = Form.useForm();
+  const [tagsError, setTagsError] = useState(false);
+
+  const props = {
+    action: '//jsonplaceholder.typicode.com/posts/', // TODO: change this to the server url
+    listType: 'picture',
+    previewFile(file) {
+      console.log('Your upload file:', file);
+      // Your process logic. Send the file to server and show the uploading progress bar etc.
+      // return fetch('', {
+      //   method: 'POST',
+      //   body: file,
+      // })
+      //   .then((res) => res.json())
+      //   .then(({ thumbnail }) => thumbnail);
+    },
+  };
+
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleSubmit = ({ body }) => {
+    // logic to submit form to server
+    form.resetFields();
+    setVisible(false);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+    form.resetFields();
+  };
+
   return (
-    <div>
-      <Form layout="inline" style={{ justifyContent: 'space-evenly' }}>
-        <Form.Item>
-          <Input.TextArea
-            placeholder="Write your message here"
-            bordered={false}
-            autoSize={{ minRows: 1, maxRows: 6 }}
-            style={{
-              fontSize: 18,
-              width: '70vw',
-              backgroundColor: 'lightgray',
-              padding: '2px',
-              resize: 'none',
-              borderRadius: 30,
-              boxShadow: '0 0 0 2px #000',
-              border: '10px solid transparent',
-            }}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Button style={{ width: '50px', height: '50px' }} icon={<SendOutlined style={{ fontSize: '25px' }} />} size="large" shape="circle" type="primary" />
-        </Form.Item>
-      </Form>
-    </div>
+    <>
+      <Button
+        onClick={showModal}
+        style={{
+          backgroundColor: '#0000',
+          border: 'none',
+          position: 'fixed',
+          bottom: '100px',
+          right: '50px',
+        }}
+      >
+        <PlusCircleTwoTone style={{ fontSize: '50px' }} color="blue" />
+      </Button>
+      <Modal
+        visible={visible}
+        onOk={form.submit}
+        onCancel={handleCancel}
+        closable={false}
+        okButtonProps={{ size: 'large', shape: 'circle', type: 'primary' }}
+        okText={<UpOutlined style={{ fontSize: '25px' }} />}
+        cancelButtonProps={{ size: 'large', shape: 'circle' }}
+        cancelText={<CloseOutlined style={{ fontSize: '25px' }} />}
+      >
+        <h1 style={{ fontSize: '20px', fontWeight: 'bold' }}>Create a post</h1>
+        <Form
+          form={form}
+          onFinish={handleSubmit}
+          style={{ padding: '20px' }}
+          header={(
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <h1 style={{ fontSize: '20px', fontWeight: 'bold' }}>Create a post</h1>
+            </div>
+          )}
+        >
+          <Form.Item
+            rules={[
+              {
+                required: true,
+                message: 'Please enter body of post',
+              },
+            ]}
+            style={{ marginBottom: '20px', height: '200px' }}
+          >
+            <TextEditor />
+          </Form.Item>
+          <Form.Item
+            label="Images"
+            name="images"
+            style={{ marginBottom: '0px' }}
+          >
+            <Upload {...props}>
+              <Button icon={<UploadOutlined />}>Upload</Button>
+            </Upload>
+          </Form.Item>
+          <Form.Item
+            label="Tags"
+            validateStatus={tagsError ? 'error' : ''}
+            help={tagsError ? 'Tag cannot be longer than 10 characters' : null}
+          >
+            <Tags trigger={setTagsError} />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
   );
 }
 
