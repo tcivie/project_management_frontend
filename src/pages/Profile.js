869447ebc37ee435d526afa6f754ec3437ddeb64
barmor12 <77288623+barmor12@users.noreply.github.com>
2023-06-05 21:30:37 +0300

    add serach history

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,94 @@
-import React, { useState } from 'react';
-import { Space, Button, Avatar, Upload, message, Modal, Skeleton, List, Empty, Menu } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Space, Button, Avatar, Upload, message, Modal, Skeleton, List, Empty, Menu, Input } from 'antd';
 import {
   UserOutlined,
+  UploadOutlined,
   DeleteOutlined,
   SaveOutlined,
   EditOutlined,
   ProfileOutlined,
   StockOutlined,
+  HistoryOutlined,
 } from '@ant-design/icons';
+import Cookies from 'js-cookie';
 
 const { SubMenu } = Menu;
+const { TextArea } = Input;
+
+function EditProfileForm({ formData, onCancel, onSave }) {
+  const [editData, setEditData] = useState(formData);
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handlePasswordChange = (e) => {
+    setNewPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newPassword !== confirmPassword) {
+      message.error('Passwords do not match.');
+      return;
+    }
+    try {
+      const response = await fetch('/api/users', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${Cookies.get('token')}`,
+        },
+        body: JSON.stringify({ ...editData, password: newPassword }),
+        credentials: 'include',
+      });
+      if (response.ok) {
+        const data = await response.json();
+        onSave(data);
+        message.success('Profile updated successfully.');
+      } else {
+        message.error('Failed to update profile.');
+      }
+    } catch (error) {
+      message.error('Failed to update profile.');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="nickname">Nickname:</label>
+        <Input name="nickname" value={editData.nickname} onChange={handleInputChange} />
+      </div>
+      <div>
+        <label htmlFor="newPassword">New Password:</label>
+        <Input.Password name="newPassword" value={newPassword} onChange={handlePasswordChange} />
+      </div>
+      <div>
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <Input.Password name="confirmPassword" value={confirmPassword} onChange={handleConfirmPasswordChange} />
+      </div>
+      <div style={{ marginTop: 10 }}>
+        <Button type="primary" htmlType="submit">
+          Save
+        </Button>
+        <Button style={{ marginLeft: 10 }} onClick={onCancel}>
+          Cancel
+        </Button>
+      </div>
+    </form>
+  );
+}
 
 function Profile() {
   const [profilePicture, setProfilePicture] = useState(null);
@@ -17,6 +96,87 @@ function Profile() {
   const [followStatus, setFollowStatus] = useState(false);
   const [followersVisible, setFollowersVisible] = useState(false);
   const [followingVisible, setFollowingVisible] = useState(false);
+  const [editFormVisible, setEditFormVisible] = useState(false);
+  const [editFormData, setEditFormData] = useState({
+    username: '',
+    email: '',
+    nickname: '',
+  });
+  const [searchHistory, setSearchHistory] = useState([]); // State for storing search history
+  const [uploadHistory, setUploadHistory] = useState([]); // State for storing upload history
+  const [savedContents, setSavedContents] = useState([]); // State for storing saved contents
+
+  useEffect(() => {
+    // Fetch and set the search history
+    const fetchSearchHistory = async () => {
+      try {
+        const response = await fetch('/api/search-history', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${Cookies.get('token')}`,
+          },
+          credentials: 'include',
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setSearchHistory(data);
+        } else {
+          message.error('Failed to fetch search history.');
+        }
+      } catch (error) {
+        message.error('Failed to fetch search history.');
+      }
+    };
+
+    // Fetch and set the upload history
+    const fetchUploadHistory = async () => {
+      try {
+        const response = await fetch('/api/upload-history', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${Cookies.get('token')}`,
+          },
+          credentials: 'include',
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setUploadHistory(data);
+        } else {
+          message.error('Failed to fetch upload history.');
+        }
+      } catch (error) {
+        message.error('Failed to fetch upload history.');
+      }
+    };
+
+    // Fetch and set the saved contents
+    const fetchSavedContents = async () => {
+      try {
+        const response = await fetch('/api/saved-contents', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${Cookies.get('token')}`,
+          },
+          credentials: 'include',
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setSavedContents(data);
+        } else {
+          message.error('Failed to fetch saved contents.');
+        }
+      } catch (error) {
+        message.error('Failed to fetch saved contents.');
+      }
+    };
+
+    fetchSearchHistory();
+    fetchUploadHistory();
+    fetchSavedContents();
+  }, []);
 
   const handleProfilePictureUpload = (file) => {
     setProfilePicture(file);
@@ -66,16 +226,24 @@ function Profile() {
     setFollowingVisible(false);
   };
 
-  const followersData = [];
-  const followingData = [];
+  const handleEditProfileSave = (updatedData) => {
+    setEditFormVisible(false);
+    setEditFormData((prevData) => ({
+      ...prevData,
+      ...updatedData,
+    }));
+  };
+
+  const handleSearchHistoryClick = () => {
+    console.log('Search History clicked');
+  };
+
+  const followersData = []; // Dummy data for followers
+  const followingData = []; // Dummy data for following
 
   return (
     <div style={{ display: 'flex' }}>
-      <Menu
-        style={{ width: 256, marginRight: 20, marginTop: 100 }}
-        defaultSelectedKeys={['1']}
-        mode="vertical"
-      >
+      <Menu style={{ width: 256, marginRight: 20, marginTop: 100 }} defaultSelectedKeys={['1']} mode="vertical">
         <Menu.Item key="1" icon={<EditOutlined />}>
           Edit post
         </Menu.Item>
@@ -85,16 +253,23 @@ function Profile() {
         <Menu.Item key="3" icon={<SaveOutlined />}>
           Saved content
         </Menu.Item>
-        <Menu.Item key="4" icon={<ProfileOutlined />}>
+        <Menu.Item key="4" icon={<ProfileOutlined />} onClick={() => setEditFormVisible(true)}>
           Edit Profile
         </Menu.Item>
         <Menu.Item key="5" icon={<StockOutlined />}>
           Statistics
         </Menu.Item>
+        <Menu.Item key="6" icon={<HistoryOutlined />} onClick={handleSearchHistoryClick}>
+          Search History
+        </Menu.Item>
       </Menu>
       <div style={{ flex: 1 }}>
         <div style={{ textAlign: 'center' }}>
-          <Avatar size={200} icon={profilePicture ? null : <UserOutlined />} src={profilePicture && URL.createObjectURL(profilePicture)} />
+          <Avatar
+            size={200}
+            icon={profilePicture ? null : <UserOutlined />}
+            src={profilePicture && URL.createObjectURL(profilePicture)}
+          />
           <div style={{ marginTop: 10 }}>
             <Button type={followStatus ? 'default' : 'primary'} onClick={handleFollowToggle}>
               {followStatus ? 'Unfollow' : 'Follow'}
@@ -111,7 +286,7 @@ function Profile() {
               beforeUpload={() => false}
               onChange={handleUploadChange}
             >
-              <Button>
+              <Button icon={<UploadOutlined />}>
                 {profilePicture ? 'Change Picture' : 'Upload Picture'}
               </Button>
             </Upload>
@@ -133,12 +308,7 @@ function Profile() {
 
         <Modal visible={followersVisible} onCancel={handleFollowersClose} centered footer={null}>
           {followersData.length > 0 ? (
-            <List
-              dataSource={followersData}
-              renderItem={(item) => (
-                <List.Item>{item}</List.Item>
-              )}
-            />
+            <List dataSource={followersData} renderItem={(item) => <List.Item>{item}</List.Item>} />
           ) : (
             <Empty description="No followers." />
           )}
@@ -146,16 +316,46 @@ function Profile() {
 
         <Modal visible={followingVisible} onCancel={handleFollowingClose} centered footer={null}>
           {followingData.length > 0 ? (
-            <List
-              dataSource={followingData}
-              renderItem={(item) => (
-                <List.Item>{item}</List.Item>
-              )}
-            />
+            <List dataSource={followingData} renderItem={(item) => <List.Item>{item}</List.Item>} />
           ) : (
             <Empty description="Not following anyone." />
           )}
         </Modal>
+
+        <Modal visible={editFormVisible} onCancel={() => setEditFormVisible(false)} footer={null}>
+          <EditProfileForm
+            formData={editFormData}
+            onCancel={() => setEditFormVisible(false)}
+            onSave={handleEditProfileSave}
+          />
+        </Modal>
+
+        {/* Display the list of saved contents */}
+        <div style={{ marginTop: 40 }}>
+          {savedContents.length > 0 ? (
+            <List dataSource={savedContents} renderItem={(item) => <List.Item>{item}</List.Item>} />
+          ) : (
+            <Empty description="No saved contents." />
+          )}
+        </div>
+
+        {/* Display the search history */}
+        <div style={{ marginTop: 40 }}>
+          {searchHistory.length > 0 ? (
+            <List dataSource={searchHistory} renderItem={(item) => <List.Item>{item}</List.Item>} />
+          ) : (
+            <Empty description="No search history." />
+          )}
+        </div>
+
+        {/* Display the upload history */}
+        <div style={{ marginTop: 40 }}>
+          {uploadHistory.length > 0 ? (
+            <List dataSource={uploadHistory} renderItem={(item) => <List.Item>{item}</List.Item>} />
+          ) : (
+            <Empty description="No upload history." />
+          )}
+        </div>
       </div>
     </div>
   );
--- src/utils/fetchWikiData.js
+++ src/utils/fetchWikiData.js
@@ -14,3 +14,5 @@ export async function fetchOpenGraphData(wikiId) {
       throw new Error('Network response was not ok.');
     });
 }
+
+
